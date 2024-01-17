95fe23ec0fa7229c9b6cbd01286f760649ec3fd1
Xin Lu <xin@fortue.com>
2024-01-17 21:15:33 +0800

    feat: 🎸 Virtual React List

--- src/components/TweetCard/index.js
+++ src/components/TweetCard/index.js
@@ -1,63 +1,15 @@
 import Bar from '@components/Bar';
+import { OBJECT_TYPES } from '@components/Bar/constants';
 import ImageCard from '@components/ImageCard';
 import { useGoTo } from '@utils/hooks';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
-import { OBJECT_TYPES } from '@components/Bar/constants';
 import style from './index.module.scss';
 
-const tweet = {
-  id: 1, // 推文id
-  user: {
-    id: 2, // 发送该推文的用户id
-    username: 'EpikGaming', // 发送该推文的用户名
-    nickname: 'EpikGamingT3', // 发送该推文的用户昵称
-    avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该推文的用户头像地址
-  }, // 发送该推文的用户信息
-  comments: [
-    {
-      id: 1, // 评论id
-      tweet_id: 1, // 评论的推文id
-      user: {
-        id: 1, // 发送该评论的用户id
-        username: 'admin', // 发送该评论的用户名
-        nickname: 'EpikGamingT3', // 发送该评论的用户昵称
-        avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该评论的用户头像地址
-      }, // 发送该评论的用户信息
-      content: 'Test!', // 该评论的文本内容
-      created_at: '2021-12-22T15:03:52.662052Z', // 该评论的创建时间
-      likes_count: 10, // 该评论点赞数
-      has_liked: false, // 当前登录的用户是否点赞了这个评论，true：当前登录的用户点赞了这条评论，false：当前登录的用户没有点赞这条评论
-    },
-    {
-      id: 2, // 评论id
-      tweet_id: 1, // 评论的推文id
-      user: {
-        id: 1, // 发送该评论的用户id
-        username: 'admin', // 发送该评论的用户名
-        nickname: 'EpikGamingT3', // 发送该评论的用户昵称
-        avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该评论的用户头像地址
-      }, // 发送该评论的用户信息
-      content: 'Test!', // 该评论的文本内容
-      created_at: '2021-12-22T15:03:52.662052Z', // 该评论的创建时间
-      likes_count: 10, // 该评论点赞数
-      has_liked: false, // 当前登录的用户是否点赞了这个评论，true：当前登录的用户点赞了这条评论，false：当前登录的用户没有点赞这条评论
-    },
-  ], // 该推文的评论集
-  created_at: '2021-12-18T07:38:01.699129Z', // 该推文的创建时间
-  content: 'Id values are not mutable. Any id value in the body of your PUT or PATCH request will be ignored. Only a value set in a POST request will be respected, but only if not already taken.', // 该推文的文本内容
-  likes: [], // 哪些用户点赞了这条推文
-  likes_count: 10, // 该推文的点赞数
-  comments_count: 122, // 该推文的评论数
-  has_liked: false, // 当前登录的用户是否点赞了这条推文，true：当前登录的用户点赞了这条推文，false：当前登录的用户没有点赞这条推文
-  photo_urls: ['https://mooc-drop.oss-cn-beijing.aliyuncs.com/20200607085521_Czt8N.gif',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZuKXKJeqzfVVrwwS6IZ0NfZUwaxMoXiJkeya7tUM04zl3BJtbbbx2rThPKxwpXeufwbc&usqp=CAU',
-    'https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2021/08/12/realtime/13315182.jpg',
-    // 'https://mooc-drop.oss-cn-beijing.aliyuncs.com/20200607085521_Czt8N.gif',
-  ], // 该推文的图片地址集
-};
-
-const TweetCard = () => {
+const TweetCard = ({
+  dataSource: tweet,
+}) => {
   const goTo = useGoTo();
 
   return (
@@ -96,6 +48,11 @@ const TweetCard = () => {
       </div>
     </div>
   );
+};
+
+TweetCard.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  dataSource: PropTypes.object.isRequired,
 };
 
 export default TweetCard;
--- src/containers/Tweets/index.js
+++ src/containers/Tweets/index.js
@@ -1,17 +1,183 @@
-import TweetCard from '@components/TweetCard';
+import { useEffect, useState } from 'react';
+import {
+  CellMeasurer, CellMeasurerCache, List, WindowScroller,
+} from 'react-virtualized';
 
+import TweetCard from '@components/TweetCard';
 import style from './index.module.scss';
+
+const tweet1 = {
+  id: 1, // 推文id
+  user: {
+    id: 2, // 发送该推文的用户id
+    username: 'EpikGaming', // 发送该推文的用户名
+    nickname: 'EpikGamingT3', // 发送该推文的用户昵称
+    avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该推文的用户头像地址
+  }, // 发送该推文的用户信息
+  comments: [
+    {
+      id: 1, // 评论id
+      tweet_id: 1, // 评论的推文id
+      user: {
+        id: 1, // 发送该评论的用户id
+        username: 'admin', // 发送该评论的用户名
+        nickname: 'EpikGamingT3', // 发送该评论的用户昵称
+        avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该评论的用户头像地址
+      }, // 发送该评论的用户信息
+      content: 'Test!', // 该评论的文本内容
+      created_at: '2021-12-22T15:03:52.662052Z', // 该评论的创建时间
+      likes_count: 10, // 该评论点赞数
+      has_liked: false, // 当前登录的用户是否点赞了这个评论，true：当前登录的用户点赞了这条评论，false：当前登录的用户没有点赞这条评论
+    },
+    {
+      id: 2, // 评论id
+      tweet_id: 1, // 评论的推文id
+      user: {
+        id: 1, // 发送该评论的用户id
+        username: 'admin', // 发送该评论的用户名
+        nickname: 'EpikGamingT3', // 发送该评论的用户昵称
+        avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该评论的用户头像地址
+      }, // 发送该评论的用户信息
+      content: 'Test!', // 该评论的文本内容
+      created_at: '2021-12-22T15:03:52.662052Z', // 该评论的创建时间
+      likes_count: 10, // 该评论点赞数
+      has_liked: false, // 当前登录的用户是否点赞了这个评论，true：当前登录的用户点赞了这条评论，false：当前登录的用户没有点赞这条评论
+    },
+  ], // 该推文的评论集
+  created_at: '2021-12-18T07:38:01.699129Z', // 该推文的创建时间
+  content: 'I rock!', // 该推文的文本内容
+  likes: [], // 哪些用户点赞了这条推文
+  likes_count: 10, // 该推文的点赞数
+  comments_count: 122, // 该推文的评论数
+  has_liked: false, // 当前登录的用户是否点赞了这条推文，true：当前登录的用户点赞了这条推文，false：当前登录的用户没有点赞这条推文
+  photo_urls: [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZuKXKJeqzfVVrwwS6IZ0NfZUwaxMoXiJkeya7tUM04zl3BJtbbbx2rThPKxwpXeufwbc&usqp=CAU',
+    'https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2021/08/12/realtime/13315182.jpg',
+  ], // 该推文的图片地址集
+};
+
+const tweet2 = {
+  id: 1, // 推文id
+  user: {
+    id: 2, // 发送该推文的用户id
+    username: 'EpikGaming', // 发送该推文的用户名
+    nickname: 'EpikGamingT3', // 发送该推文的用户昵称
+    avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该推文的用户头像地址
+  }, // 发送该推文的用户信息
+  comments: [
+    {
+      id: 1, // 评论id
+      tweet_id: 1, // 评论的推文id
+      user: {
+        id: 1, // 发送该评论的用户id
+        username: 'admin', // 发送该评论的用户名
+        nickname: 'EpikGamingT3', // 发送该评论的用户昵称
+        avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该评论的用户头像地址
+      }, // 发送该评论的用户信息
+      content: 'Test!', // 该评论的文本内容
+      created_at: '2021-12-22T15:03:52.662052Z', // 该评论的创建时间
+      likes_count: 10, // 该评论点赞数
+      has_liked: false, // 当前登录的用户是否点赞了这个评论，true：当前登录的用户点赞了这条评论，false：当前登录的用户没有点赞这条评论
+    },
+    {
+      id: 2, // 评论id
+      tweet_id: 1, // 评论的推文id
+      user: {
+        id: 1, // 发送该评论的用户id
+        username: 'admin', // 发送该评论的用户名
+        nickname: 'EpikGamingT3', // 发送该评论的用户昵称
+        avatar_url: 'https://img.shoufaw.com/wp-content/uploads/2020/10/aEjURn.jpg', // 发送该评论的用户头像地址
+      }, // 发送该评论的用户信息
+      content: 'Test!', // 该评论的文本内容
+      created_at: '2021-12-22T15:03:52.662052Z', // 该评论的创建时间
+      likes_count: 10, // 该评论点赞数
+      has_liked: false, // 当前登录的用户是否点赞了这个评论，true：当前登录的用户点赞了这条评论，false：当前登录的用户没有点赞这条评论
+    },
+  ], // 该推文的评论集
+  created_at: '2021-12-18T07:38:01.699129Z', // 该推文的创建时间
+  content: 'Id values are not mutable. Any id value in the body of your PUT or PATCH request will be ignored. Only a value set in a POST request will be respected, but only if not already taken.', // 该推文的文本内容
+  likes: [], // 哪些用户点赞了这条推文
+  likes_count: 10, // 该推文的点赞数
+  comments_count: 122, // 该推文的评论数
+  has_liked: false, // 当前登录的用户是否点赞了这条推文，true：当前登录的用户点赞了这条推文，false：当前登录的用户没有点赞这条推文
+  photo_urls: ['https://mooc-drop.oss-cn-beijing.aliyuncs.com/20200607085521_Czt8N.gif',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZuKXKJeqzfVVrwwS6IZ0NfZUwaxMoXiJkeya7tUM04zl3BJtbbbx2rThPKxwpXeufwbc&usqp=CAU',
+    'https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2021/08/12/realtime/13315182.jpg',
+    'https://mooc-drop.oss-cn-beijing.aliyuncs.com/20200607085521_Czt8N.gif',
+  ], // 该推文的图片地址集
+};
+
+const defaultData = [];
+
+for (let i = 0; i < 100; i += 1) {
+  if (Math.random() < 0.5) {
+    defaultData.push(tweet1);
+  } else {
+    defaultData.push(tweet2);
+  }
+}
+
+const cache = new CellMeasurerCache({
+  fixedWidth: true,
+  minHeight: 200,
+});
+
+const noRowsRenderer = () => '加载中...';
 
 /**
 * Tweets homepage
 */
-const Tweets = () => (
-  <div className={style.container}>
-    <TweetCard />
-    <TweetCard />
-    <TweetCard />
-    <TweetCard />
-  </div>
-);
+const Tweets = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(defaultData);
+  }, []);
+
+  const rowRenderer = ({
+    key, style: sy, index, parent,
+  }) => (
+    <CellMeasurer
+      cache={cache}
+      columnIndex={0}
+      key={key}
+      rowIndex={index}
+      parent={parent}
+    >
+      {({ registerChild }) => (
+        <div style={sy} key={key} ref={registerChild}>
+          <TweetCard dataSource={data[index]} />
+        </div>
+      )}
+    </CellMeasurer>
+  );
+
+  return (
+    <div className={style.container}>
+      <WindowScroller>
+        {({
+          height, width, isScrolling, registerChild, onChildScroll, scrollTop,
+        }) => (
+          <div ref={registerChild}>
+            <List
+              isScrolling={isScrolling}
+              onScroll={onChildScroll}
+              scrollTop={scrollTop}
+              autoHeight
+              height={height}
+              deferredMeasurementCache={cache}
+              rowHeight={cache.rowHeight}
+              overscanRowCount={2}
+              noRowsRenderer={noRowsRenderer}
+              rowCount={data.length}
+              rowRenderer={rowRenderer}
+              width={width}
+            />
+          </div>
+        )}
+      </WindowScroller>
+    </div>
+  );
+};
 
 export default Tweets;
